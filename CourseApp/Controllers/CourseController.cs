59b58571fcb66ccee55e9a593443c7a113a2b2a4
Hakan Bağlan <baglanhakan42@gmail.com>
2021-09-12 16:27:51 +0300

    Add project files.

---
+++ CourseApp/Controllers/CourseController.cs
@@ -0,0 +1,30 @@
+﻿using CourseApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseApp.Controllers
+{
+    public class CourseController : Controller
+    {
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour > 12 ? "God Afternoon" : "Goodmorning";
+            return View("MyView");
+        }
+
+        [HttpGet]
+        public ViewResult Apply()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult Apply(Student student)
+        {
+            return View();
+        }
+    }
+}
---
+++ CourseApp/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using CourseApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var course = new Course();
+            course.Id = 1;
+            course.Name = "Asp .Net Core 2 Kursu";
+            return View(course);
+        }
+    }
+}
